e394aa99f6a6b6e4f09192215d6c62bb614ba2cb
Enes Susan <menric.drago@gmail.com>
2024-03-12 17:41:47 +0300

    Add todo logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from './todo.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private ts: TodoService) { }
+  todo = {
+    title: '',
+    status: 'pending'
+  }
+
   title = 'todoApp';
+  todos: any[] = [];
+
+
+
+  getTodos() {
+    this.ts.getTodos().subscribe((data: any) => {
+      console.log(data);
+      this.todos = data;
+    }
+    );
+  }
+sendData() {
+  this.ts.createTodo(this.todo).subscribe((data: any) => {
+    console.log(data);
+    this.getTodos();
+  });
 }
+
+  ngOnInit() {
+    this.getTodos();
+  }
+  deleteTodo(id: number) {
+    this.ts.deleteTodo(id.toString()).subscribe((data: any) => {
+      console.log(data);
+      this.getTodos();
+    });
+  }
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoService } from './todo.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
+    
     AppComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
+     HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
