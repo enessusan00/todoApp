6597104caa65753258de8d3af172863b837fc1ae
Enes Susan <menric.drago@gmail.com>
2024-04-24 11:15:20 +0300

    add form validation for signup component

--- src/app/auth/pages/signup/signup.component.ts
+++ src/app/auth/pages/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../auth.service';
+import { Router } from '@angular/router';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +9,85 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private formBuilder: FormBuilder
 
+  ) {
+  }
+  username: string = '';
+  email: string = '';
+  password: string = '';
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  ngOnInit(): void {
+    
+    this.form = this.formBuilder.group(
+      {
+        username: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40),
+          ],
+        ],
+
+      },
+    );
+    this.auth.logout().subscribe();
+  }
+  submitted = false;
+  passInputType: string = 'password';
+
+  togglePassType() {
+    this.passInputType === 'password' ? this.passInputType = 'text' : this.passInputType = 'password';
+  }
+  showError = false;
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+  onSubmit() {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+    this.showError = false
+    this.signUp();
+  }
+ 
+  errorText: string = ''
+  
+  signUp() {
+    this.auth.signup(this.form.value.username, this.form.value.email, this.form.value.password).subscribe(
+      {
+        next: (n) => {
+          this.login(  this.form.value.email, this.form.value.password);
+        },
+        error: (e) => {
+          this.errorText = e.error;
+          this.showError = true;
+        }
+      }
+    );
+  }
+  login(email:string,password:string) {
+    this.auth.authenticate( email,password).subscribe({
+      next: (n) => {
+        this.auth.isAdmin ? this.router.navigate(['/admin-dashboard']) :
+        this.router.navigate(['/dashboard']);
+      },
+      error: (e) => {
+        this.errorText = e.error || e.message;
+        this.showError = true;
+      }
+    })
+  }
 }
